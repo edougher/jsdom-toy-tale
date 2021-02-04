da6d9de5de9b6f64f14397bc000cd49ef760f905
edougher <ejdougher@gmail.com>
2021-02-04 14:05:53 -0600

    added form Post functionality

--- src/index.js
+++ src/index.js
@@ -8,14 +8,68 @@ document.addEventListener("DOMContentLoaded", () => {
 
     addToy = !addToy;
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyFormContainer.style.display = "block"
+  
     } else {
       toyFormContainer.style.display = "none";
     }
   });
 
   lostToys()
+  createFormListener() 
 });
+
+function createFormListener() {
+const form = document.querySelector('form')
+ form.addEventListener('submit', function(e) { 
+   e.preventDefault()
+   console.log(e)
+   const newToy = {
+     name: e.target['name'].value,
+     image: e.target['image'].value,
+   }
+   const reqObj = {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json'
+     },
+     body: JSON.stringify(newToy)
+    }
+
+   fetch('http://localhost:3000/toys', reqObj)
+   .then((resp) => resp.json())
+   .then(toy => {
+     form.reset()
+     //console.log(toy)
+     //lostToys()
+   })
+ })
+}
+
+//function renderToy(toy) {
+//const toyCollection = document.getElementById("toy-collection")
+//const toyElement = document.createElement('div')
+//  toyElement.className = 'card'
+//  const h2 = document.createElement('h2')
+//  const img = document.createElement('img')
+//  const pTag = document.createElement('p')
+//  const btnTag = document.createElement('button')
+//  pTag.innerHTML = `${toy.likes} Likes` 
+//  btnTag.className = 'like-btn'
+//  btnTag.innerHTML = 'Like <3'
+//  h2.innerHTML = toy.name
+//  img.src = toy.image
+//  toyElement.appendChild(h2)
+//  toyElement.appendChild(img)
+//  toyElement.appendChild(pTag)
+//  toyElement.appendChild(btnTag)
+//  
+//  toyCollection.appendChild(toyElement)
+//}
+
+
+
+
 function lostToys() {
   let toysURL = "http://localhost:3000/toys"
   fetch(toysURL)
@@ -29,6 +83,7 @@ function renderCards(toys) {
   toys.forEach(toy => {
     const toyElement = document.createElement('div')
     toyElement.className = 'card'
+
     const h2 = document.createElement('h2')
     const img = document.createElement('img')
     const pTag = document.createElement('p')
@@ -45,8 +100,10 @@ function renderCards(toys) {
     toyElement.appendChild(btnTag)
     
     toyCollection.appendChild(toyElement)
-    console.log(toyCollection)
+    //console.log(toyCollection)
   })
 
-  //console.log(toyElement)
+
+
+
 }
