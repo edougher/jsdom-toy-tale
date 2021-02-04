5c6dcef4be243e71dd744865733e4d30b23d9c3a
edougher <ejdougher@gmail.com>
2021-02-04 11:14:27 -0600

    finished toy cards

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
+
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -12,4 +13,40 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  lostToys()
 });
+function lostToys() {
+  let toysURL = "http://localhost:3000/toys"
+  fetch(toysURL)
+  .then(resp => resp.json())
+  .then(json => renderCards(json))
+}
+function renderCards(toys) {
+  const toyCollection = document.getElementById("toy-collection")
+  
+  
+  toys.forEach(toy => {
+    const toyElement = document.createElement('div')
+    toyElement.className = 'card'
+    const h2 = document.createElement('h2')
+    const img = document.createElement('img')
+    const pTag = document.createElement('p')
+    const btnTag = document.createElement('button')
+    pTag.innerHTML = `${toy.likes} Likes` 
+    btnTag.className = 'like-btn'
+    btnTag.innerHTML = 'Like <3'
+
+    h2.innerHTML = toy.name
+    img.src = toy.image
+    toyElement.appendChild(h2)
+    toyElement.appendChild(img)
+    toyElement.appendChild(pTag)
+    toyElement.appendChild(btnTag)
+    
+    toyCollection.appendChild(toyElement)
+    console.log(toyCollection)
+  })
+
+  //console.log(toyElement)
+}
